e12abd71b74e3b80eba0f28f575065086ea4192b
Vanilson Albuquerque <van.alb@outlook.com>
2023-02-03 18:00:04 -0300

    3 commit

--- main.js
+++ main.js
@@ -2,18 +2,6 @@ const page = document.body.style;
 page.background = "#2E2D2D"; //Background escuro;
 page.color = 'white'; //Letra branca;
 
-let buttonAvaliacoes = document.getElementsByClassName('fichava')[0];
-let info_ava = document.getElementsByClassName('texto_ava')[0];
-buttonAvaliacoes.addEventListener('click', function () {
-    if (info_ava.classList.contains('texto_ava')) {
-        info_ava.ClassList.remove('texto_ava');
-        info_ava.style.display = "none";
-        info_ava.style.display = "block";
-    } else {
-        info_ava.classList.add('texto_ava');
-    }
-});
-
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -22,10 +10,10 @@ $('.slider-for').slick({
     asNavFor: '.slider-nav'
 });
 $('.slider-nav').slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
+    dots: false,
+    centerMode: false,
+    focusOnSelect: false
 });
